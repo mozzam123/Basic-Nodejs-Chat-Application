7308302836b51dc681beff50bb531cdf0a3fc9e4
mozzam inamdar <mozzam607@gmail.com>
2023-09-06 22:49:56 +0530

    Worked on Broadcasting message

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
+io.on('connection', (socket) => {
+
+  console.log('User connected!!!');
+  io.emit('chat message', `user Connected:  ${socket.id}`)
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+
+  socket.on('disconnect', () =>{
+    console.log('User disconnected');
+    io.emit('chat message', `user Disconnected:  ${socket.id}`)
+  })  
+});
+
+
+server.listen(8000, () => {
+  console.log('listening on *:8000');
+});
